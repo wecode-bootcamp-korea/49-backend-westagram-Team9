e8632fbf23bf0c727489ffb0d1c05f9d9641c550
B3lla <sjin09300@gmail.com>
2023-09-05 23:42:00 +0900

    FEATURE: Error handling

--- eunchae/app.js
+++ eunchae/app.js
@@ -42,11 +42,64 @@ const users = async(req, res) => {
 
 const userCreate = async(req, res) => {
   try {
-    const user = req.body;
+    // const user = req.body;
+    const { name, email, password } = req.body; //구조분해할당
 
-    await myDataSource.query(`INSERT INTO users (name, email, password) VALUES ('${user.name}', '${user.email}', '${user.password}')`,
+    // 이메일에 @가 없을 때
+    if (!email.includes('@')) {
+      const error = new Error('EMAIL_INVALID');
+      error.statusCode = 403;
+      throw error;
+    }
+
+    // name, email, password가 다 입력되지 않은 경우
+    if (name === undefined || email === undefined || !password) {
+      const error = new Error('KEY_ERROR');
+      error.statusCode = 400;
+      throw error;
+    }
+
+    // 비밀번호가 8자 이내일 때
+    if (password.length < 8) {
+      const error = new Error('INVALID_PASSWORD');
+      error.statusCode = 400;
+      throw error;
+    }
+
+    // 중복된 이메일이 있을 때
+    const emailCheck = await myDataSource.query(`SELECT * FROM users WHERE email LIKE '${email}'`);
+
+    if (emailCheck.length !== 0) {
+      const error = new Error('DUPLICATE_EMAIL');
+      error.statusCode = 400;
+      throw error;
+    }
+
+    // 비밀번호에 특수문자 없을 때
+    const specialChar = /[\{\}\[\]\/?.,;:|\)*~`!^\-_+<>@\#$%&\\\=\(\'\"]/g;
+    
+    if (!specialChar.test(password)) {
+      const error = new Error('NOT_FOUND_SPECIAL_CHAR');
+      error.statusCode = 400;
+      throw error;
+    }
+
+    // await myDataSource.query(`INSERT INTO users (name, email, password) VALUES ('${name}', '${email}', '${password}')`);
+    await myDataSource.query(`INSERT INTO users (name, email, password) VALUES ('${name}', '${email}', '${password}')`,
     (err, rows) => {
       return res.status(201).json({ message: "userCreated" });
+    });
+  } catch(err) {
+    console.log(err);
+    return res.status(err.statusCode).json({ message: err.message });
+  }
+}
+
+const postCreate = async(req, res) => {
+  try {
+    const userPost = req.body;
+    await myDataSource.query(`INSERT INTO posts (title, content, user_id) VALEUS ('${userPost.title}', '${userPost.content}', '${userPost.user_id}')`, (err, rows) => {
+      return res.status(201).json({ message: "postCreated" });
     });
   } catch(err) {
     console.log(err);
@@ -56,6 +109,7 @@ const userCreate = async(req, res) => {
 app.get('/', test);
 app.get('/user', users);
 app.post('/user', userCreate);
+app.post('/posts', postCreate);
 
 const port = 8000;
 app.listen(port, () => {
